cff1d336af623692a5e968c9a55fd6ca96b63640
HK <user@HK.local>
2019-11-04 19:51:37 +0900

    2.3

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "aedd0486703e157c7ddf657dcd4db407";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWether = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWether(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Cant't find you.", "So sad");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
+}
---
+++ Weather.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const weatherOptions = {
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"]
+  },
+  Thunderstorm: {
+    iconName: "",
+    gradient: []
+  },
+  Drizzle: {
+    iconName: "",
+    gradient: []
+  },
+  Rain: {
+    iconName: "",
+    gradient: []
+  },
+  Snow: {
+    iconName: "",
+    gradient: []
+  },
+  Atmosphere: {
+    iconName: "",
+    gradient: []
+  },
+  Clear: {
+    iconName: "",
+    gradient: []
+  },
+  Clouds: {
+    iconName: "",
+    gradient: []
+  },
+  Haze: {
+    iconName: "",
+    gradient: []
+  },
+  Mist: {
+    iconName: "",
+    gradient: []
+  },
+  Dust: {
+    iconName: "",
+    gradient: []
+  }
+};
+
+export default function Weather({ temp, condition }) {
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={weatherOptions[condition].gradient}
+    >
+      <StatusBar barStyle="light-content" />
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[condition].iconName}
+          color="white"
+        />
+
+        <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View style={styles.halfContainer} />
+    </LinearGradient>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
